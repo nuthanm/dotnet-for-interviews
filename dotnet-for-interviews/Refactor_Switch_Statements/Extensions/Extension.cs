8b7c9a02298225aafa4279290e8a9390ba5c5840
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-06-20 23:41:33 +0530

    Created a new console project where it contains number of ways we can write switch statements.

---
+++ dotnet-for-interviews/Refactor_Switch_Statements/Extensions/Extension.cs
@@ -0,0 +1,7 @@
+﻿namespace Refactor_Switch_Statements.Extensions
+{
+    public static class Extension
+    {
+        public static bool In<T>(this T value, params T[] values) => values.Contains(value);
+    }
+}
---
+++ dotnet-for-interviews/Refactor_Switch_Statements/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Refactor_Switch_Statements.Extensions;
+
+Console.WriteLine("Better Way to make switch statements");
+
+MultipleCases(10);
+MultipleCasesWithWhenMethod(35);
+MultipleCasesWithExtensions(30);
+MultipleCasesWithLogicalPattern(24);
+MultipleCasesWithRelationalPattern(25);
+
+#region TraditioanlWay_Switch
+void MultipleCases(int number)
+{
+    string result = string.Empty;
+    switch (number)
+    {
+        case 10:
+        case 11:
+        case 12:
+            result = "Numbers less than 20";
+            break;
+        case 30:
+        case 33:
+        case 35:
+            result = "Numbers between 30 and 40";
+            break;
+        default:
+            result = "Other numbers";
+            break;
+    }
+
+    Console.WriteLine(result);
+}
+#endregion
+
+
+#region Switch_With_When_Method
+void MultipleCasesWithWhenMethod(int number)
+{
+    string result = string.Empty;
+    switch (number)
+    {
+        case int n when (n > 10 & n < 20):
+            result = "Numbers less than 20";
+            break;
+        case int n when (n >= 30 & n < 40):
+            result = "Numbers between 30 and 40";
+            break;
+        default:
+            result = "Other numbers";
+            break;
+    }
+
+    Console.WriteLine(result);
+}
+#endregion
+
+
+#region SwitchWithExtensions
+void MultipleCasesWithExtensions(int number)
+{
+    var result = number switch
+    {
+        var x when x.In(10, 11, 12) => "Numbers between 10 and 20",
+        var x when x.In(20, 21, 22) => "Numbers between 20 and 30",
+        _ => "Numbers not in our range"
+    };
+
+    Console.WriteLine(result);
+}
+#endregion
+
+
+#region SwitchWithLogicalPattern
+void MultipleCasesWithLogicalPattern(int number)
+{
+    var result = number switch
+    {
+        10 or 11 or 12 => "Numbers between 10 and 20",
+        20 or 21 or 22 => "Numbers between 20 and 30",
+        _ => "Numbers not in our range"
+    };
+
+    Console.WriteLine(result);
+}
+#endregion
+
+
+#region SwitchWithRelationalPattern
+void MultipleCasesWithRelationalPattern(int number)
+{
+    var result = number switch
+    {
+        (>= 10) and (<= 20) => "Numbers between 10 and 20",
+        (>= 20) and (<= 30) => "Numbers between 20 and 30",
+        _ => "Numbers not in our range"
+    };
+
+    Console.WriteLine(result);
+}
+#endregion
