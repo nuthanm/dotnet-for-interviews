be3288bcdcb133bcb5403256c0ddbd8d659cd1e3
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-06-29 16:35:25 +0530

    1. Added two regions for sender and receiver with placeholders.

--- dotnet-for-interviews/Demos_On_ServiceBus/Program.cs
+++ dotnet-for-interviews/Demos_On_ServiceBus/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Demos on Azure Servicebus");
+using Azure.Messaging.ServiceBus;
 
+
+// Create a ServiceBusClient
+ServiceBusClient sb = new ServiceBusClient("<Add your service bus connection string>");
+
+
+#region ServiceBus_SendMessage
+Console.WriteLine("Demos on Azure Servicebus - Begin Sending");
+
+// Create a ServiceBusSender
+ServiceBusSender sbSender = sb.CreateSender("<Add_Your_Queue_Or_TopicName");
+
+// Prepare a message
+ServiceBusMessage sbMessage = new ServiceBusMessage("<Add your message_or_json_string");
+
+// Send a message
+await sbSender.SendMessageAsync(sbMessage);
+
+Console.WriteLine("Demos on Azure Servicebus - End");
+
+#endregion
+
+#region ServiceBus_ReceiveMessage
+Console.WriteLine("Demos on Azure Servicebus - Begin Receving");
+
+// Create a ServiceBusSender
+ServiceBusReceiver sbReceiver = sb.CreateReceiver("<Add_Your_Queue_Or_TopicName");
+
+// Send a message
+var sbReceivedMessage = await sbReceiver.ReceiveMessageAsync();
+
+Console.WriteLine($"Message what we received: {sbReceivedMessage.Body}");
+
+Console.WriteLine("Demos on Azure Servicebus - End");
+
+#endregion
