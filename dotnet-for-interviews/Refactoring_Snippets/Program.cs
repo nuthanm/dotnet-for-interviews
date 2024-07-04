b63b4ab9c24b1f411272b81487980dcdc096f813
nuthanmurarysetty <inbox.nuthan@gmail.com>
2024-07-04 16:02:47 +0530

    Reactoring_Snippets is a new project added in this solution

---
+++ dotnet-for-interviews/Refactoring_Snippets/Program.cs
@@ -0,0 +1,27 @@
+﻿
+#region Input_Value_is_Not_Null
+
+// Old Code
+string inputCorrelationId = "a8f2cbf7-0388-4ef1-9f2b-fbe064f845db";
+var id = !string.IsNullOrEmpty(inputCorrelationId)
+? Guid.Parse(inputCorrelationId) : Guid.NewGuid();
+Console.WriteLine($"Old: {id}");
+// Optmized Code
+var optimizedApproachId = Guid.TryParse(inputCorrelationId, out var correlationId) ? correlationId : Guid.NewGuid();
+Console.WriteLine($"New: {optimizedApproachId}");
+
+#endregion
+
+#region Input_Value_is_null
+
+// Old Code
+string inputCorrelationIdIsnull = null;
+var idWithNewGuid = !string.IsNullOrEmpty(inputCorrelationIdIsnull)
+? Guid.Parse(inputCorrelationIdIsnull) : Guid.NewGuid();
+Console.WriteLine($"Old: {id}");
+
+// Optmized Code
+var optimizedApproachIdTryParseNull = Guid.TryParse(inputCorrelationIdIsnull, out var correlationIdNew) ? correlationIdNew : Guid.NewGuid();
+Console.WriteLine($"New: {optimizedApproachIdTryParseNull}");
+
+#endregion
